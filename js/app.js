dd8f79a3068f14be3dcdc96a8a2223179740d09d
Cameron Doane <camdoane@uw.edu>
2015-12-05 15:11:55 -0800

    [broken] start work on loading image

--- js/app.js
+++ js/app.js
@@ -19,5 +19,34 @@ angular.module("Stego", ["ui.router"])
             $urlRouterProvider.otherwise("/welcome");
     })
     .controller("StegoController", function($scope){
-        console.log("Stego is up and running!");
+        "use strict";
+
+        $scope.submitForm = function(){
+            var file = document.getElementById("imageInput").files[0];
+            if(file){
+                var read = new FileReader();
+                read.onloadend = function(e){
+                    //get the url result
+                    var data = read.result
+                    // put that url in an img tag (may not be necessary?)
+                    var imageElement = document.createElement("img");
+                    imageElement.setAttribute("src", data);
+                    console.log(imageElement);
+                    // put that image into a canvas
+                    var canvas = document.createElement('canvas');
+                    canvas.setAttribute("width", imageElement.width);
+                    canvas.setAttribute("height", imageElement.height);
+                    var context = canvas.getContext('2d');
+                    context.drawImage(imageElement, 0, 0 );
+                    var rawData = context.getImageData(0,0,imageElement.width, imageElement.height);
+                    // read in the data as arrays
+                    var buf8 = new Uint8ClampedArray(rawData);
+                    console.log(buf8);
+                    var buf32 = new Uint32Array(rawData);
+                    console.log(buf32);
+                }
+                read.readAsDataURL(file);
+                console.log("reading file");
+            }
+        }
     });
