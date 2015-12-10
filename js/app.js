598d516a8df69fbfb02500eef9865f9edf35cf30
CamD67 <camdoane@uw.edu>
2015-12-10 14:44:07 -0800

    added in more decode functionality

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ angular.module("Stego", ["ui.router"])
             });
             $urlRouterProvider.otherwise("/welcome");
     })
-    .controller("StegoController", function($scope){
+    .controller("StegoController", function($scope, $state){
         "use strict";
 
         $scope.loading = false;
@@ -44,18 +44,46 @@ angular.module("Stego", ["ui.router"])
                     // getting a reference to the data to speed up references
                     var data = rawData.data;
                     var manipulator = new Manipulator(data.buffer, canvas.width, canvas.height);
-                    manipulator.encode("test");
+                    manipulator.encode(document.getElementById("encoding-text").value);
                     rawData.data.set(manipulator.buf8);
                     context.putImageData(rawData, 0, 0);
-                    imageElement.setAttribute("src", canvas.toDataURL().toString());
-                    document.getElementById("mainDiv").appendChild(imageElement);
-                    console.log("Output: (RGBA format)");
-                    console.log(data);
+                    document.getElementById("outputImage").setAttribute("src", canvas.toDataURL().toString());
                     $scope.loading = false;
                 };
                 $scope.loading = true;
                 read.readAsDataURL(file);
+            }
+        };
 
+        $scope.decode = function () {
+            var file = document.getElementById("imageInput").files[0];
+            if(file){
+                var read = new FileReader();
+                read.onloadend = function(e){
+                    //get the url result
+                    var result = read.result;
+                    // put that url in an img element
+                    var imageElement = document.createElement("img");
+                    imageElement.setAttribute("src", result);
+
+                    // put that image into a canvas
+                    var canvas = document.createElement('canvas');
+                    canvas.setAttribute("width", imageElement.naturalWidth);
+                    canvas.setAttribute("height", imageElement.naturalHeight);
+                    var context = canvas.getContext('2d');
+                    context.drawImage(imageElement, 0, 0);
+                    var rawData = context.getImageData(0, 0, canvas.width, canvas.height);
+                    // getting a reference to the data to speed up references
+                    var data = rawData.data;
+                    var manipulator = new Manipulator(data.buffer, canvas.width, canvas.height);
+                    var output = manipulator.decode();
+                    rawData.data.set(manipulator.buf8);
+                    context.putImageData(rawData, 0, 0);
+                    document.getElementById("outputText").innerHTML(output);
+                    $scope.loading = false;
+                };
+                $scope.loading = true;
+                read.readAsDataURL(file);
             }
         };
 
